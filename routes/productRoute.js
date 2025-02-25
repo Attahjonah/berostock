8bf68676bc18cb2e1ee93310cd9c80ac127f74fd
Jonah Attah <attahjonah93@gmail.com>
2025-02-25 02:35:32 +0100

    update

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -6,8 +6,8 @@ const route = Router()
 route.post('/', productController.CreateProduct)
 route.get('/', productController.GetAllProduct)
 route.get('/:productId', productController.GetProduct)
-route.update('/:productId', productController.UpdateProduct)
-route.delete('/:productId', productController.DeleteProduct)
+route.put('/:productId', productController.UpdateProduct)
+route.delete('/:productId', productController.DeleteProduct) 
 
 
 module.exports = route
